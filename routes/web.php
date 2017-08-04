89fc700109a724e1e0b51c69bd3021c6a22e7312
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-08-03 23:35:57 -0400

    Making the logout route 'GET' instead of 'POST'

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::post('/post/{slug}/comment', 'PostCommentController@store')
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');

a826156ef5e198014568287b54ec1fc0a80c63ef
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-08-03 23:53:08 -0400

    Removing pre-wrap from post show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -15,7 +15,7 @@
 
 <section class="section">
     <div class="container">
-        <article style="white-space: pre-wrap;">{!! $post->body !!}</article>
+        <article>{!! $post->body !!}</article>
     </div>
 </section>
 <section class="section">

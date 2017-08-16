1bfdf156b62564f81b276d5752ce68850e03974f
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-08-15 23:19:08 -0400

    Added home controller test

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HomeControllerTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testIndex()
     {
         $response = $this->get('/');

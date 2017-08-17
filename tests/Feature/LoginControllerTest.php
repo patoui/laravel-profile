1a17a8b4a4b4837901a54f04a1965e95bec3aada
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-08-16 22:36:41 -0400

    Added test for login controller

---
+++ tests/Feature/LoginControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test to display the login form
+     */
+    public function testShowLoginForm()
+    {
+        $response = $this->get('login');
+
+        $response->assertStatus(200);
+    }
+
+    public function testLogin()
+    {
+        // Arrange
+        $user = factory(User::class)->create([
+            'email' => 'patrique.ouimet@gmail.com',
+            'password' => bcrypt('testpass'),
+        ]);
+
+        // Act
+        $response = $this->post(
+            'login',
+            [
+                'email' => 'patrique.ouimet@gmail.com',
+                'password' => 'testpass',
+            ]
+        );
+
+        // Assert
+        $response->assertStatus(302)
+            ->assertRedirect('admin/dashboard');
+    }
+
+    public function testLogout()
+    {
+        // Arrange
+        $user = factory(User::class)->create([
+            'email' => 'patrique.ouimet@gmail.com',
+            'password' => bcrypt('testpass'),
+        ]);
+
+        // Act
+        $response = $this->actingAs($user)->get('logout');
+
+        // Assert
+        $response->assertStatus(302)
+            ->assertRedirect('/');
+    }
+}

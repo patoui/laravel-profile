7b5503308742ebab62a245832d8c9547e86b37af
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-06-12 08:18:24 -0400

    Added logging config

---
+++ config/logging.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['syslog', 'slack'],
+        ],
+
+        'syslog' => [
+            'driver' => 'syslog',
+            'level' => 'debug',
+        ],
+
+        'slack' => [
+            'driver' => 'slack',
+            'url' => env('LOG_SLACK_WEBHOOK_URL'),
+            'username' => 'Laravel Log',
+            'emoji' => ':boom:',
+            'level' => 'critical',
+        ],
+    ],
+];

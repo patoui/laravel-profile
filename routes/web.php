719f16abd4a057789ad382e0b49acdd81e2ac9d2
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-07-15 16:13:43 -0400

    Removed unused code/comments

--- routes/web.php
+++ routes/web.php
@@ -43,21 +43,6 @@ Route::get('register', 'Auth\RegisterController@showRegistrationForm')
 Route::post('register', 'Auth\RegisterController@register')
     ->name('register.store');
 
-// Password Reset
-// Route::get(
-//     'password/reset',
-//     'Auth\ForgotPasswordController@showLinkRequestForm'
-// )->name('password.request');
-// Route::post(
-//     'password/email',
-//     'Auth\ForgotPasswordController@sendResetLinkEmail'
-// )->name('password.email');
-// Route::get(
-//     'password/reset/{token}',
-//     'Auth\ResetPasswordController@showResetForm'
-// )->name('password.reset');
-// Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
 // Socialite (Github)
 Route::get('auth/github', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/github/callback', 'Auth\LoginController@handleProviderCallback');

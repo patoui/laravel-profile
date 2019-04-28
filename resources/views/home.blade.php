d18f4f158641eca67feb09f6962b828b70df4514
Patrique Ouimet <patrique.ouimet@gmail.com>
2019-04-28 15:21:37 -0400

    Fixed contact form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Patrique Ouimet')
 
 @section('content')
-<span class="block mt-2 mb-5 font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Hello world! My name is Patrique Ouimet :D</span>
+<span class="w-full block mt-2 font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Hello world!</span>
+<h1 class="w-full block mb-4 font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">My name is Patrique Ouimet :D</h1>
 <p class="block mb-5 leading-relaxed">I'm a developer who currently works with: PHP (Laravel), JavaScript (AngularJS, VueJS), and CSS (Sass). I have an obssession with learning, whether it’s to sharpen my existing skills or to explore new ones such as: Ruby (Ruby on Rails), Python (Django), AdonisJS, React, etc. I also enjoy running, hiking, biking, weight lifting, soccer, and basketball.</p>
 <p class="block mb-5 leading-relaxed">As of late I’ve decided to try myself at blogging to share some tips and tricks I’ve learned during my time as a developer, whether they be technical, interpersonel, or otherwise; my hope is that fellow developers may learn something from my blog.</p>
 <p class="block mb-5 leading-relaxed">If you have any suggestions or materials that would assist me in becoming a better developer or to help me write better content, please don’t hesitate to contact me on Twitter <a href="https://twitter.com/patoui2" class="text-blue-500">@patoui2</a> or in the form below.</p>
@@ -41,7 +42,7 @@
             @endif
         </div>
         <div class="flex items-center justify-center">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Send!</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Send!</button>
         </div>
     </form>
 </div>

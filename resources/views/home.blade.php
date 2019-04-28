c7704518624f641d61f3609a511f6090fbaf969a
Patrique Ouimet <patrique.ouimet@gmail.com>
2019-04-28 15:36:14 -0400

    Fixed contact form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
         <h2>Contact Me</h2>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2 {{ $errors->has('name') ? 'border-red-500' : '' }}" for="name"></label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name">
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Name">
             @if ($errors->has('name'))
             <p class="text-red-500 text-xs italic">{{ $errors->first('name') }}</p>
             @endif

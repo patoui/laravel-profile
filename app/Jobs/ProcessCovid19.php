370dbfc85a3fb90808ad8a4de09d7fdea0973b76
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-04-10 11:48:52 -0400

    Added better handling for failed jobs

--- app/Jobs/ProcessCovid19.php
+++ app/Jobs/ProcessCovid19.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Covid19;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -13,8 +14,16 @@ class ProcessCovid19 implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public int $retryAfter = 10;
+    public int $tries      = 3;
+
     public function handle(): void
     {
         (new Covid19())->process(true);
+    }
+
+    public function failed(Exception $exception): void
+    {
+        (new Covid19())->process(false);
     }
 }

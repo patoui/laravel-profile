a2c480e2b8901a8ef9d951c5695063016706c3b2
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-04-18 11:47:11 -0400

    Order methods alphabetically

--- app/Covid19.php
+++ app/Covid19.php
@@ -11,6 +11,27 @@ use Zttp\Zttp;
 
 class Covid19
 {
+    private function makeRequest(string $url): array
+    {
+        $response = Zttp::get($url);
+
+        if (!$response->isOk()) {
+            $status = $response->status();
+            $body = $response->body();
+            throw new RuntimeException("Error occurred while fetching data {$url} ({$status}): {$body}");
+        }
+
+        $data = $response->json();
+
+        if (!is_array($data)) {
+            $status = $response->status();
+            $body = $response->body();
+            throw new RuntimeException("No data returned {$url} ({$status}): {$body}");
+        }
+
+        return $data;
+    }
+
     public function process(string $last_slug = null): void
     {
         $countries = $this->getCountries(true);
@@ -32,13 +53,6 @@ class Covid19
         }
     }
 
-    public function setCountries(): array
-    {
-        $data = $this->makeRequest('https://api.covid19api.com/countries');
-        Cache::put('covid19_countries', $data);
-        return $data;
-    }
-
     public function getCountries(bool $update_cache = false): array
     {
         if ($update_cache) {
@@ -53,23 +67,6 @@ class Covid19
         return array_search($country_slug, array_column($this->getCountries(), 'Slug'), true);
     }
 
-    public function getCountryLabelBySlug(string $country_slug): string
-    {
-        return array_column($this->getCountries(), 'Country')[$this->getCountryIndex($country_slug)] ?? '';
-    }
-
-    public function setCountryConfirmed(string $country_slug): array
-    {
-        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/confirmed");
-        foreach ($data as $key => $set) {
-            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
-            $current_cases = $set['Cases'] ?? 0;
-            $data[$key]['Difference'] = $current_cases - $prev_cases;
-        }
-        Cache::put('covid19_confirmed_' . $country_slug, $data);
-        return $data;
-    }
-
     public function getCountryConfirmed(string $country_slug, bool $update_cache = false): array
     {
         if ($update_cache) {
@@ -80,18 +77,6 @@ class Covid19
             'covid19_confirmed_' . $country_slug,
             fn() => $this->setCountryConfirmed($country_slug)
         );
-    }
-
-    public function setCountryDeaths(string $country_slug): array
-    {
-        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/deaths");
-        foreach ($data as $key => $set) {
-            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
-            $current_cases = $set['Cases'] ?? 0;
-            $data[$key]['Difference'] = $current_cases - $prev_cases;
-        }
-        Cache::put('covid19_deaths_' . $country_slug, $data);
-        return $data;
     }
 
     public function getCountryDeaths(string $country_slug, bool $update_cache = false): array
@@ -106,16 +91,20 @@ class Covid19
         );
     }
 
-    public function setCountryRecovered(string $country_slug): array
+    public function getCountryDetails(array $country_slugs): array
     {
-        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/recovered");
-        foreach ($data as $key => $set) {
-            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
-            $current_cases = $set['Cases'] ?? 0;
-            $data[$key]['Difference'] = $current_cases - $prev_cases;
+        $details = [];
+
+        foreach ($country_slugs as $country_slug) {
+            $details[$country_slug] = $this->getCountryLabelBySlug($country_slug);
         }
-        Cache::put('covid19_recovered_' . $country_slug, $data);
-        return $data;
+
+        return $details;
+    }
+
+    public function getCountryLabelBySlug(string $country_slug): string
+    {
+        return array_column($this->getCountries(), 'Country')[$this->getCountryIndex($country_slug)] ?? '';
     }
 
     public function getCountryRecovered(string $country_slug, bool $update_cache = false): array
@@ -130,24 +119,46 @@ class Covid19
         );
     }
 
-    private function makeRequest(string $url): array
+    public function setCountries(): array
     {
-        $response = Zttp::get($url);
+        $data = $this->makeRequest('https://api.covid19api.com/countries');
+        Cache::put('covid19_countries', $data);
+        return $data;
+    }
 
-        if (!$response->isOk()) {
-            $status = $response->status();
-            $body = $response->body();
-            throw new RuntimeException("Error occurred while fetching data {$url} ({$status}): {$body}");
+    public function setCountryConfirmed(string $country_slug): array
+    {
+        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/confirmed");
+        foreach ($data as $key => $set) {
+            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
+            $current_cases = $set['Cases'] ?? 0;
+            $data[$key]['Difference'] = $current_cases - $prev_cases;
         }
+        Cache::put('covid19_confirmed_' . $country_slug, $data);
+        return $data;
+    }
 
-        $data = $response->json();
-
-        if (!is_array($data)) {
-            $status = $response->status();
-            $body = $response->body();
-            throw new RuntimeException("No data returned {$url} ({$status}): {$body}");
+    public function setCountryDeaths(string $country_slug): array
+    {
+        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/deaths");
+        foreach ($data as $key => $set) {
+            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
+            $current_cases = $set['Cases'] ?? 0;
+            $data[$key]['Difference'] = $current_cases - $prev_cases;
         }
+        Cache::put('covid19_deaths_' . $country_slug, $data);
+        return $data;
+    }
 
+    public function setCountryRecovered(string $country_slug): array
+    {
+        $data = $this->makeRequest("https://api.covid19api.com/total/country/{$country_slug}/status/recovered");
+        foreach ($data as $key => $set) {
+            $prev_cases = $data[$key - 1]['Cases'] ?? 0;
+            $current_cases = $set['Cases'] ?? 0;
+            $data[$key]['Difference'] = $current_cases - $prev_cases;
+        }
+        Cache::put('covid19_recovered_' . $country_slug, $data);
         return $data;
     }
 }

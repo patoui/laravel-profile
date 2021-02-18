3fff17c3ce9f4348205d34ce843a3153c37a5432
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-02-18 08:38:58 -0500

    Removed unused files

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,4 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [ require('tailwindcss') ])
     .purgeCss()
-    .version()
-    .copy('resources/js/google-analytics.js', 'public/js/google-analytics.js');
+    .version();

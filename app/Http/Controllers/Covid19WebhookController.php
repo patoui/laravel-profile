9d20cf7d2d46538f3e98e54fb496437981716902
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-02-19 08:21:43 -0500

    Returning 204, no way currently to de-register webhook

--- app/Http/Controllers/Covid19WebhookController.php
+++ app/Http/Controllers/Covid19WebhookController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\ProcessCovid19;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Cache;
 
 class Covid19WebhookController extends Controller
 {
     public function index(): JsonResponse
     {
-        Cache::put('webhook_triggered', ['timestamp' => Carbon::now()->toIso8601String()]);
-        ProcessCovid19::dispatch();
-        return new JsonResponse('Successfully triggered');
+        return new JsonResponse(null, 204);
     }
 }

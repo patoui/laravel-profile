564ecdb1571a65018d7ad9ca56f8a4e9de4155ba
Patrique Ouimet <patrique.ouimet@gmail.com>
2022-05-15 10:35:02 -0400

    Updated the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,52 @@
 @section('title', 'Patrique Ouimet')
 
 @section('content')
-<span class="w-full block mt-2 font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Hello world!</span>
-<h1 class="w-full block mb-4 font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">My name is Patrique Ouimet :D</h1>
-<p class="block mb-5 leading-relaxed">I'm a developer who currently works with: PHP (Laravel), JavaScript (VueJS, ReactJS), and CSS. I have an obsession with learning, whether it’s to sharpen my existing skills or to explore new ones such as: Go, Elixir, Ruby (Ruby on Rails), Python (Django), React, etc. I also enjoy running, hiking, biking, weight lifting, soccer, and basketball.</p>
-<p class="block mb-5 leading-relaxed">I’ve decided to try myself at blogging to share some tips and tricks I’ve learned during my time as a developer, whether they be technical, interpersonal, or otherwise; my hope is that fellow developers may learn something from my blog.</p>
-<p class="block mb-5 leading-relaxed">If you have any suggestions or materials that would assist me in becoming a better developer or to help me write better content, please don’t hesitate to contact me on Twitter <a href="https://twitter.com/patoui2" class="text-blue-600">@patoui2</a>.</p>
+<h1 class="w-full block mt-3 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Hi there 👋, I'm Pat! 😄</h1>
+<p class="block mb-5 leading-relaxed">I'm a Staff Software Engineer primarily working in/with PHP but love discovering/trying new technologies (currently learning Go lang and TypeScript)!</p>
+
+<h2 class="w-full block mt-3 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl">About me</h2>
+<ul class="list-disc ml-5">
+    <li>
+        <p class="block mb-2 leading-relaxed">🔭 I’m currently working on a variety of PHP projects utilizing some great open source projects! <a class="text-blue-600 visited:text-purple-600" href="https://laravel.com/" title="Laravel">Laravel</a>, <a class="text-blue-600 visited:text-purple-600" href="https://www.slimframework.com/" title="Slim">Slim</a>, <a class="text-blue-600 visited:text-purple-600" href="https://docs.guzzlephp.org/en/stable/" title="Guzzle">Guzzle</a></p>
+    </li>
+    <li>
+        <p class="block mb-2 leading-relaxed">🌱 I’m currently learning <a class="text-blue-600 visited:text-purple-600" href="https://golang.org/">Go lang</a>! Big thanks to <a class="text-blue-600 visited:text-purple-600" href="https://twitter.com/GoTimeFM" title="GoTime">GoTime</a> and <a class="text-blue-600 visited:text-purple-600" href="https://threedots.tech/go-with-the-domain/">Go With Domain</a></p>
+    </li>
+    <li>
+        <p class="block mb-2 leading-relaxed">🦕 I'm also currently learning <a class="text-blue-600 visited:text-purple-600" href="https://www.typescriptlang.org/" title="TypeScript">TypeScript</a> with <a class="text-blue-600 visited:text-purple-600" href="https://deno.land" title="Deno">Deno</a></p>
+    </li>
+    <li>
+        <p class="block mb-2 leading-relaxed">✌️ I’m looking to collaborate on anything OSS but haven't quite found something I'm very interested in yet (open to suggestions!)</p>
+    </li>
+    <li>
+        <p class="block mb-2 leading-relaxed">❤️ Beer🍺️, Coffee ☕️, Cycling 🚴‍♂️️, Hiking 🗻️, Camping 🏕️</p>
+    </li>
+</ul>
+
+<h2 class="w-full block mt-5 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl">📫 Where to find me</h2>
+
+<ul class="list-disc ml-5">
+    <li class="mb-2">🐙️&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://github.com/patoui" title="GitHub">GitHub</a></li>
+    <li class="mb-2">🐦️&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://twitter.com/patoui2" title="Twitter">Twitter</a></li>
+    <li class="mb-2">◻️&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://polywork.com/patoui" title="Polywork">Polywork</a></li>
+    <li class="mb-2">📸️&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://instagram.com/patoui2" title="Instagram">Instagram</a></li>
+    <li class="mb-2">👨‍💻&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://linkedin.com/in/patrique-ouimet-8b2aa969" title="LinkedIn">LinkedIn</a></li>
+    <li class="mb-2">🐘️&nbsp;<a class="text-blue-600 visited:text-purple-600" href="https://phpc.social/web/accounts/106705262503935129" title="PHP Social">PHP Social</a></li>
+</ul>
+
+<h2 class="w-full block mt-5 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl">🔭️ Technologies</h2>
+
+<p class="mb-2">Not listed in any particular order and likely missing a few 😅️</p>
+
+<ul class="list-disc ml-5">
+    <li class="mb-2">💻 &nbsp; PHP | Laravel | JavaScript | Vue | React</li>
+    <li class="mb-2">🌐 &nbsp; HTML | CSS | SASS | Bootstrap | Tailwind</li>
+    <li class="mb-2">🛢 &nbsp; MySQL | Redis | Clickhouse | SQLite</li>
+    <li class="mb-2">🔧 &nbsp; PHPStorm | Sublime Text | Visual Studio Code | Git</li>
+    <li class="mb-2">🖥 &nbsp; Nginx | Apache | Caddy | Docker | Vagrant</li>
+    <li class="mb-2">🛸️ &nbsp; Linux | Bash | AWS | Digital Ocean</li>
+</ul>
+
 @endsection
 
 @include('vendor.flash.message')

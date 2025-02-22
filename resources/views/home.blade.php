691f20e5b302a58f4903da66816dda2ba2a1a680
Patrique Ouimet <patrique.ouimet@gmail.com>
2025-02-22 08:17:09 -0500

    chore: update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,24 +12,24 @@
 
 @section('content')
 <h1 class="w-full block mt-3 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Hi there 👋, I'm Pat! 😄</h1>
-<p class="block mb-5 leading-relaxed">I'm a Senior Product Engineer primarily working in/with PHP but love discovering/trying new technologies (currently learning Go lang and TypeScript)!</p>
+<p class="block mb-5 leading-relaxed">I'm a Senior Product Engineer primarily working in/with PHP but love discovering/trying new technologies! I've been a software engineer for over a decade and have built systems for a variety of products and industries, such as: marketing, contesting, lead acquisition, automotive, and healthcare</p>
 
 <h2 class="w-full block mt-3 mb-2 font-bold text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl">About me</h2>
 <ul class="w-full block list-disc ml-5">
     <li>
-        <p class="block mb-2 leading-relaxed">🔭 I’m currently working on a variety of PHP projects utilizing some great open source projects! <a class="text-blue-600 visited:text-purple-600" href="https://laravel.com/" title="Laravel">Laravel</a>, <a class="text-blue-600 visited:text-purple-600" href="https://www.slimframework.com/" title="Slim">Slim</a>, <a class="text-blue-600 visited:text-purple-600" href="https://docs.guzzlephp.org/en/stable/" title="Guzzle">Guzzle</a></p>
+        <p class="block mb-2 leading-relaxed">🔭 I'm currently working on a variety of PHP projects utilizing some great open source projects! <a class="text-blue-600 visited:text-purple-600" href="https://laravel.com/" title="Laravel">Laravel</a>, <a class="text-blue-600 visited:text-purple-600" href="https://www.slimframework.com/" title="Slim">Slim</a>, <a class="text-blue-600 visited:text-purple-600" href="https://docs.guzzlephp.org/en/stable/" title="Guzzle">Guzzle</a></p>
     </li>
     <li>
-        <p class="block mb-2 leading-relaxed">⚛ I’m currently using <a class="text-blue-600 visited:text-purple-600" href="https://reactjs.org/">React</a> and <a class="text-blue-600 visited:text-purple-600" href="https://reactnative.dev/">React Native</a> to make a mobile app!</p>
+        <p class="block mb-2 leading-relaxed">🏠 I'm currently working on a <a class="text-blue-600 visited:text-purple-600" href="https://github.com/patoui/laravel-clickhouse">Laravel package</a> which wraps the <a class="text-blue-600 visited:text-purple-600" href="https://clickhouse.com/">ClickHouse</a> extension by <a class="text-blue-600 visited:text-purple-600" href="https://github.com/SeasX/SeasClick">SeasClick</a>!</p>
     </li>
     <li>
-        <p class="block mb-2 leading-relaxed">🌱 I’m currently learning <a class="text-blue-600 visited:text-purple-600" href="https://golang.org/">Go lang</a>! Big thanks to <a class="text-blue-600 visited:text-purple-600" href="https://x.com/GoTimeFM" title="GoTime">GoTime</a> and <a class="text-blue-600 visited:text-purple-600" href="https://threedots.tech/go-with-the-domain/">Go With Domain</a></p>
+        <p class="block mb-2 leading-relaxed">🌱 Facinated by <a class="text-blue-600 visited:text-purple-600" href="https://golang.org/">Go lang</a>! Big thanks to <a class="text-blue-600 visited:text-purple-600" href="https://x.com/GoTimeFM" title="GoTime">GoTime</a> and <a class="text-blue-600 visited:text-purple-600" href="https://threedots.tech/go-with-the-domain/">Go With Domain</a></p>
     </li>
     <li>
-        <p class="block mb-2 leading-relaxed">🦕 I'm also currently learning <a class="text-blue-600 visited:text-purple-600" href="https://www.typescriptlang.org/" title="TypeScript">TypeScript</a> with <a class="text-blue-600 visited:text-purple-600" href="https://deno.land" title="Deno">Deno</a></p>
+        <p class="block mb-2 leading-relaxed">🦕 Intrigued by the <a class="text-blue-600 visited:text-purple-600" href="https://deno.com" title="Deno">Deno</a> runtime, looking for excuses to build something with it!</p>
     </li>
     <li>
-        <p class="block mb-2 leading-relaxed">✌️ I’m looking to collaborate on anything OSS but haven't quite found something I'm very interested in yet (open to suggestions!)</p>
+        <p class="block mb-2 leading-relaxed">✌️ I'm looking to collaborate on anything OSS but haven't quite found something I'm very interested in yet (open to suggestions!)</p>
     </li>
     <li>
         <p class="block mb-2 leading-relaxed">❤️ Beer🍺️, Coffee ☕️, Cycling 🚴‍♂️️, Hiking 🗻️, Camping 🏕️</p>
@@ -54,7 +54,7 @@
 <ul class="w-full block list-disc ml-5">
     <li class="mb-2">💻 &nbsp; PHP | Go | Laravel | JavaScript | Vue | React | React Native | Java</li>
     <li class="mb-2">🌐 &nbsp; HTML | CSS | SASS | Bootstrap | Tailwind</li>
-    <li class="mb-2">🛢 &nbsp; MySQL | Redis | Typescript | Clickhouse | SQLite | Mongo</li>
+    <li class="mb-2">🛢 &nbsp; MySQL | Redis | Typescript | ClickHouse | SQLite | Mongo | AWS DynamoDB | AWS Glue</li>
     <li class="mb-2">🔧 &nbsp; PHPStorm | Sublime Text | Visual Studio Code | Zed | Neovim | Git</li>
     <li class="mb-2">🖥 &nbsp; Nginx | Apache | Caddy | Docker | Vagrant</li>
     <li class="mb-2">🛸️ &nbsp; Linux | Bash | AWS | Digital Ocean</li>

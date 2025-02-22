65765d5556834034197ab2b8ca248048b8ff6dcc
Patrique Ouimet <patrique.ouimet@gmail.com>
2025-02-21 20:40:29 -0500

    chore: duster

--- app/User.php
+++ app/User.php
@@ -7,7 +7,6 @@ namespace App;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
